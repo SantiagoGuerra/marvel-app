8bf674abd09523980c9952fa7d9523e3c0614319
Santiago Guerra <guerrasantiago19@gmail.com>
2020-08-03 14:54:06 -0500

    Create characters reducer

---
+++ src/reducers/characters.js
@@ -0,0 +1,10 @@
+export const characters = (state = [], action) => {
+  switch (action.type) {
+    case 'GET_CHARACTERS':
+      return action.payload.characters;
+    default:
+      return state;
+  }
+};
+
+export default characters;
