db0c9b0235d45a4eee1054d30f8bce93efdc50fd
Santiago Guerra <guerrasantiago19@gmail.com>
2020-08-03 22:09:39 -0500

    Create filter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getDataForFeed } from './middlewares';
+import { changeCurrentCategory } from './actions';
 
 function App() {
   const state = useSelector(state => state);
+  const { currentCategory } = state.filter;
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getDataForFeed('characters'));
-  }, [dispatch]);
+    dispatch(getDataForFeed(currentCategory));
+  }, [dispatch, currentCategory]);
 
   return (
     <div>
+
+      <button onClick={() => {
+        dispatch(changeCurrentCategory('stories'));
+      }}
+      >
+        Change
+      </button>
 
       {console.dir(state)}
 
