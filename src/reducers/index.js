50264225ac3598647493aa37ff3facc27c29d5cd
Santiago Guerra <guerrasantiago19@gmail.com>
2020-08-03 22:03:15 -0500

    Add filter object to redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import feedReducer from './feed';
+import filterReducer from './filter';
 
 const rootReducers = combineReducers({
   feed: feedReducer,
+  filter: filterReducer,
 });
 
 export default rootReducers;
