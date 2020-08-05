8e71c95088f584b9abf2c9afaade48ee03449aa2
Santiago Guerra <guerrasantiago19@gmail.com>
2020-08-05 16:14:32 -0500

    Improve character information component

--- src/components/CharacterBasicInfo.js
+++ src/components/CharacterBasicInfo.js
@@ -1,27 +1,94 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, List, Heading, Text, Image, Tabs, Tab, Meter} from "grommet";
+
+
+
+const ShowTabInformation = ({dataTab}) => {
+
+
+  useEffect(() => {
+  }, [])
+
+
+  const available = dataTab?.available
+
+  return (
+    <>
+
+   
+
+      <Heading
+        margin={{
+          top: 'medium'
+        }}
+      >
+        Available: {available}
+      </Heading>
+      <List
+        primaryKey="name"
+        secondaryKey="percent"
+        data={dataTab?.items}
+      />
+
+
+    </>
+  )
+}
+
 
 function CharacterBasicInfo({ data }) {
 
 
-  const {
-    name,
-    thumbnail,
-    description
-  } = data;
+  const [dat, setData] = useState({})
 
+  useEffect(() => {
+    setData(data)
+  }, [data])
+
+
+  const {thumbnail, name, comics, events, series, stories, description} = dat
+
+  console.log(comics);
 
   return (
-    <div>
-      <img src={`${ thumbnail?.path }.${ thumbnail?.extension }`} alt={ name }/>
-      <h2>
-        { name }
-      </h2>
+    <>
+      <Box direction="column" wrap gap="medium">
+        <Box height="medium">
+          <Image
+            fit="cover"
+            src={`${thumbnail?.path}.${thumbnail?.extension}`}
+          />
+        </Box>
+        <Box direction="column" gap="small" basis="80%">
+          <Heading>{name}</Heading>
 
-      <p>
-        {description}
-      </p>
-    </div>
-  )
+          <Text>{description}</Text>
+        </Box>
+      </Box>
+      <Tabs
+        alignControls="start"
+        margin={{
+          top: 'xlarge'
+        }}
+      >
+        <Tab title="Comics">
+          <ShowTabInformation dataTab={comics}/>
+        </Tab>
+        <Tab title="Events">
+        <ShowTabInformation dataTab={events}/>
+
+        </Tab>
+        <Tab title="Series">
+        <ShowTabInformation dataTab={series}/>
+
+        </Tab>
+        <Tab title="Stories">
+        <ShowTabInformation dataTab={stories}/>
+
+        </Tab>
+      </Tabs>
+    </>
+  );
 }
 
-export default CharacterBasicInfo
+export default CharacterBasicInfo;
