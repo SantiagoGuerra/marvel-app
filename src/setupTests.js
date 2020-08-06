67e66eae531097566c6a3d24aac98c3bf899758d
Santiago Guerra <guerrasantiago19@gmail.com>
2020-08-06 15:43:31 -0500

    Configure Enzyme to test

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,6 @@
-import '@testing-library/jest-dom/extend-expect';
+import { createSerializer } from 'enzyme-to-json';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
