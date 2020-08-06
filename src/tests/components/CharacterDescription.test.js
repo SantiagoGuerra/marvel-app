fbfee1e5c30ec167b062274f0f3ce53e8d2562ff
Santiago Guerra <guerrasantiago19@gmail.com>
2020-08-06 16:54:56 -0500

    Test should to render the CharacterDescription Component correctly

---
+++ src/tests/components/CharacterDescription.test.js
@@ -0,0 +1,26 @@
+const React = require('react');
+const { shallow } = require('enzyme');
+const { default: CharacterDescription } = require('../../components/CharacterDescription');
+
+describe('Test <CharacterDescription/> Component', () => {
+  const thumbnail = {
+    path: 'https://cloudimg.io/s/XSdTer23dwedffwqa',
+    extension: 'png',
+  };
+
+  const name = 'Hulk';
+
+  const description = 'lorem ipsum';
+
+  const wrapper = shallow(
+    <CharacterDescription
+      thumbnail={thumbnail}
+      name={name}
+      description={description}
+    />,
+  );
+
+  test('should render the component correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
