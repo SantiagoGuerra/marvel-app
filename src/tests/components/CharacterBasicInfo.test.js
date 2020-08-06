ec419104feab6cd6c3762908cdb079f49968c0d1
Santiago Guerra <guerrasantiago19@gmail.com>
2020-08-06 16:43:41 -0500

    It should have a componente called Box

--- src/tests/components/CharacterBasicInfo.test.js
+++ src/tests/components/CharacterBasicInfo.test.js
@@ -3,6 +3,8 @@ const { shallow } = require('enzyme');
 const { default: CharacterBasicInfo } = require('../../components/CharacterBasicInfo');
 
 describe('Test <CharacterBasicInfo /> Component', () => {
+
+
   const thumbnail = {
     path: 'https://cloudimg.io/s/XSdTer23dwedffwqa',
     extension: 'png',
@@ -36,8 +38,10 @@ describe('Test <CharacterBasicInfo /> Component', () => {
     thumbnail, name, comics, events, series, stories, description,
   };
 
+  const wrapper = shallow(<CharacterBasicInfo data={data} />);
+
+
   test('should render the component correctly', () => {
-    const wrapper = shallow(<CharacterBasicInfo data={data} />);
     expect(wrapper).toMatchSnapshot();
   });
 
@@ -45,4 +49,20 @@ describe('Test <CharacterBasicInfo /> Component', () => {
     const wrapper = shallow(<CharacterBasicInfo data={data} />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  test('should have a component called Box', () => {
+    const Box = wrapper.find('Box');
+
+
+    expect(Box.exists()).toBe(true);
+  });
+
+
+  test('should have a component called Box', () => {
+    const Box = wrapper.find('Box');
+
+
+    expect(Box.exists()).toBe(true);
+  });
+  
 });
