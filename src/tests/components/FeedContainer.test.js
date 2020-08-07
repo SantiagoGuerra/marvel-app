e0acf0d6e29bfd783e49c4749d67a677262fc3e0
Santiago Guerra <guerrasantiago19@gmail.com>
2020-08-06 21:11:31 -0500

    Test Feed Container component

--- src/tests/components/FeedContainer.test.js
+++ src/tests/components/FeedContainer.test.js
@@ -3,7 +3,6 @@ const { shallow } = require('enzyme');
 const { default: FeedContainer } = require('../../components/FeedContainer');
 
 describe('Test <FeedContainer /> Component', () => {
-
   const feed = [];
 
   const filter = {};
@@ -12,5 +11,16 @@ describe('Test <FeedContainer /> Component', () => {
 
   test('should render the component correctly', () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('should have data as parameter', () => {
+    const wrapper = shallow(<FeedContainer feed={feed} filter={filter} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test('should have a component called Box', () => {
+    const Box = wrapper.find('Box');
+
+    expect(Box.exists()).toBe(true);
   });
 });
