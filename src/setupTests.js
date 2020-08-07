abf588b1a8aaa2d0a066bc17db296eb42eee35d3
Santiago Guerra <guerrasantiago19@gmail.com>
2020-08-07 00:19:56 -0500

    Revert "MARVEL App API with React & Redux"

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,1 @@
-import { createSerializer } from 'enzyme-to-json';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
-expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
+import '@testing-library/jest-dom/extend-expect';
