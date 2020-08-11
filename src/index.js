616a6d3ca9bbbac29d2372579f18e87d50504299
Santiago Guerra <guerrasantiago19@gmail.com>
2020-08-11 01:32:14 -0500

    Add babel to improve some issues with linter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import './styles/main.scss';
+import App from './App';
+import store from './store';
 
 ReactDOM.render(
-  <div>
-    Development
-  </div>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
